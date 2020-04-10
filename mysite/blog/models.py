02031c1fefca38a310bca6d9c9510e12c364a4e2
ABDU104 <elvin.abduyev@bertelsmann.de>
2020-04-10 23:00:11 +0200

    added all templates

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE)
+    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
     author = models.CharField(max_length=254)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
